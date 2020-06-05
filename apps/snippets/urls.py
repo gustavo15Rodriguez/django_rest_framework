843e2dc7f3f617fffc6445e5437cac0ab6058990
Gustavo A. Rodriguez <garodriguez263@gmail.com>
2020-06-05 17:52:44 -0500

    Code in snippets app refactorized.

--- apps/snippets/urls.py
+++ apps/snippets/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 from apps.snippets import views
+from rest_framework.urlpatterns import format_suffix_patterns
 
 urlpatterns = [
     path('list/', views.snippet_list),
     path('detail/<int:pk>', views.snippet_detail)
 ]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
