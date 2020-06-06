9ba3bc7170a0a0627ff76dcab83ae5c331cc5216
Gustavo A. Rodriguez <garodriguez263@gmail.com>
2020-06-05 19:22:44 -0500

    Changed functions by classes based in views.

--- apps/snippets/urls.py
+++ apps/snippets/urls.py
@@ -3,8 +3,8 @@ from apps.snippets import views
 from rest_framework.urlpatterns import format_suffix_patterns
 
 urlpatterns = [
-    path('list/', views.snippet_list),
-    path('detail/<int:pk>', views.snippet_detail)
+    path('list/', views.SnippetList.as_view()),
+    path('detail/<int:pk>', views.SnippetDetail.as_view())
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
